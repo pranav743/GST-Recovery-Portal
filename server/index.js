518e4390f724d1ce57f62d4001384424decd629e
Pranav <108137959+pranav743@users.noreply.github.com>
2024-06-30 14:04:41 +0530

    Update index.js for deployment, enable CORS

--- server/index.js
+++ server/index.js
@@ -11,10 +11,35 @@ const connectDB = require("./database/db");
 // Initiall Set Up
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(cookieParser());
 app.use("/imgs", express.static(path.join(__dirname, "imgs")));
 app.use("/api", router);
+
+app.get('/', (req, res) => {
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Smart Shop Server</title>
+        </head>
+        <body>
+          <h1>Hello, this is the official server of GST Recovery Portal</h1>
+          <p>Owner: Pranav</p>
+        </body>
+      </html>
+    `;
+  
+    // Set the Content-Type header to indicate that the response is HTML
+    res.setHeader('Content-Type', 'text/html');
+  
+    // Send the HTML content as the response body
+    res.send(htmlContent);
+  });
 
 // Connecting to Database
 connectDB();
